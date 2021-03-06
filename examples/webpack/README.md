801c6682d10d6a69c6b926ea05a0037f828f5b65
Johan Holmerin <johan@holmer.in>
2021-03-06 17:13:12 +0100

    Add webpack example

---
+++ examples/webpack/README.md
@@ -0,0 +1,17 @@
+# style9-webpack-example
+
+Example using [style9](https://github.com/johanholmerin/style9) with Webpack.
+See [webpack.config.js](webpack.config.js) for config and
+[src/main.js](src/main.js) for usage.
+
+## Installation
+
+```sh
+yarn
+```
+
+## Build
+
+```sh
+yarn build
+```
---
+++ examples/webpack/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const Style9Plugin = require('style9/webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(tsx|ts|js|mjs|jsx)$/,
+        use: Style9Plugin.loader,
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      }
+    ]
+  },
+  plugins: [
+    new Style9Plugin(),
+    new MiniCssExtractPlugin({
+      filename: 'index.css'
+    })
+  ]
+};
