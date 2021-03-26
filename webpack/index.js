372b1bfb78e520d52006c6754f0a84854017f700
Johan Holmerin <johan@holmer.in>
2021-03-26 20:56:39 +0100

    Move from deprecated Webpack hook

--- webpack/index.js
+++ webpack/index.js
@@ -12,36 +12,56 @@ class Style9Plugin {
     virtualModules.apply(compiler);
 
     compiler.hooks.compilation.tap(NAME, compilation => {
-      compilation.hooks.optimizeChunkAssets.tapPromise(NAME, async chunks => {
-        const paths = Array.from(chunks)
-          .flatMap(chunk => Array.from(chunk.files))
-          .filter(path => path.match(this.test));
+      if (compilation.hooks.processAssets) {
+        compilation.hooks.processAssets.tap(
+          {
+            name: NAME,
+            stage: compiler.webpack.Compilation.PROCESS_ASSETS_STAGE_OPTIMIZE
+          },
+          assets => {
+            const paths = Object.keys(assets);
 
-        for (const path of paths) {
-          const asset = compilation.assets[path];
-          const { source, map } = asset.sourceAndMap();
-          const postcssOpts = {
-            to: path,
-            from: path,
-            map: { prev: map || false }
-          };
-          const result = processCSS(source, postcssOpts);
-
-          if (result.map) {
-            compilation.assets[path] = new SourceMapSource(
-              result.css,
-              path,
-              JSON.parse(result.map),
-              source,
-              map,
-              true
-            );
-          } else {
-            compilation.assets[path] = new RawSource(result.css);
+            this._processFiles(compilation, paths);
           }
-        }
-      });
+        );
+      } else {
+        compilation.hooks.optimizeChunkAssets.tapPromise(NAME, async chunks => {
+          const paths = Array.from(chunks)
+            .map(chunk => Array.from(chunk.files))
+            .flat();
+
+          this._processFiles(compilation, paths);
+        });
+      }
     });
+  }
+
+  _processFiles(compilation, paths) {
+    const filteredPaths = paths.filter(path => path.match(this.test));
+
+    for (const path of filteredPaths) {
+      const asset = compilation.assets[path];
+      const { source, map } = asset.sourceAndMap();
+      const postcssOpts = {
+        to: path,
+        from: path,
+        map: { prev: map || false }
+      };
+      const result = processCSS(source, postcssOpts);
+
+      if (result.map) {
+        compilation.assets[path] = new SourceMapSource(
+          result.css,
+          path,
+          JSON.parse(result.map),
+          source,
+          map,
+          true
+        );
+      } else {
+        compilation.assets[path] = new RawSource(result.css);
+      }
+    }
   }
 }
 
