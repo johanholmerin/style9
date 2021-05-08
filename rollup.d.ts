08a8f337dad968a6fb6425d0ec2daa9aec8ecc2f
Johan Holmerin <johan@holmer.in>
2021-05-08 14:02:11 +0200

    Add rollup plugin declaration file

---
+++ rollup.d.ts
@@ -0,0 +1,27 @@
+import { Plugin } from 'rollup';
+import { FilterPattern } from '@rollup/pluginutils';
+import { ParserOptions } from '@babel/core';
+
+interface BabelOptions {
+  minifyProperties?: boolean;
+}
+
+interface CommonOptions extends BabelOptions {
+  include?: FilterPattern;
+  exclude?: FilterPattern;
+  parserOptions?: ParserOptions;
+}
+
+interface FileNameOptions extends CommonOptions {
+  fileName: string;
+  name?: never;
+}
+
+interface NameOptions extends CommonOptions {
+  name: string;
+  fileName?: never;
+}
+
+type RollupOptions = FileNameOptions | NameOptions;
+
+export default function style9Plugin(rollupOptions: RollupOptions): Plugin;
