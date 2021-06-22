44bf6bb1a2a70ce56b91408383ebd8a49306171f
Johan Holmerin <johan@holmer.in>
2021-06-22 19:26:01 +0200

    Minify CSS

--- examples/nextjs/next.config.js
+++ examples/nextjs/next.config.js
@@ -1,7 +1,25 @@
 const withTM = require('next-transpile-modules')(['style9']);
 const withStyle9 = require('style9/next');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+const minify = (nextConfig = {}) => {
+  return {
+    ...nextConfig,
+    webpack(config, options) {
+      if (typeof nextConfig.webpack === 'function') {
+        config = nextConfig.webpack(config, options);
+      }
+
+      config.optimization.minimizer.push(
+        new CssMinimizerPlugin()
+      );
+
+      return config;
+    }
+  }
+};
 
 module.exports = {
-  ...withStyle9()(withTM()),
+  ...minify(withStyle9()(withTM())),
   webpack5: false,
 };
