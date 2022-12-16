409cfe90abb41370612b697c35cb290a83476747
kanno <812137533@qq.com>
2022-12-16 13:39:13 +0800

    fix: css should with correct order

--- vite.js
+++ vite.js
@@ -1,18 +1,11 @@
-const crypto = require('crypto');
+const path = require('path');
 const babel = require('@babel/core');
 const { createFilter } = require('@rollup/pluginutils');
+const processCSS = require('./src/process-css');
 const babelPlugin = require('./babel.js');
 const NAME = require('./package.json').name;
 
-const VIRTUAL_MODULE_PREFIX = `\0virtual-style9:`;
-
-function getHash(text) {
-  return crypto.createHash('sha256').update(text).digest('hex').slice(0, 8);
-}
-
-function getSecond() {
-  return Math.floor(Date.now() / 1000);
-}
+const VIRTUAL_MODULE_NAME = '@virtual-style9';
 
 module.exports = function style9Plugin(opts = {}) {
   const {
@@ -24,24 +17,31 @@ module.exports = function style9Plugin(opts = {}) {
     ...restOptions
   } = opts;
 
+  const filter = createFilter(include, exclude);
+
   const cssModules = new Map();
 
-  const filter = createFilter(include, exclude);
+  let server = null;
 
   return {
     name: NAME,
+    enforce: 'pre',
     resolveId(id) {
-      if (cssModules.has(id)) {
-        return VIRTUAL_MODULE_PREFIX + id + '?t=' + getSecond();
+      if (id === VIRTUAL_MODULE_NAME) {
+        return `\0${id}.css`;
       }
       return null;
     },
+    configureServer(viteServer) {
+      server = viteServer;
+    },
     load(id) {
-      if (id.startsWith(VIRTUAL_MODULE_PREFIX)) {
-        const [module] = id.split('?', 1);
-        const hashId = module.slice(VIRTUAL_MODULE_PREFIX.length);
-        const css = cssModules.get(hashId);
-        return css;
+      if (id.startsWith(`\0${VIRTUAL_MODULE_NAME}`)) {
+        const css = [...cssModules.values()].reduce(
+          (acc, cur) => (acc += cur),
+          ''
+        );
+        return processCSS(css, { from: undefined }).css;
       }
       return null;
     },
@@ -58,15 +58,49 @@ module.exports = function style9Plugin(opts = {}) {
       const { code, map, metadata } = res;
       const cssStr = metadata.style9 || '';
       if (cssStr) {
-        const path = `./${getHash(id)}.css`;
-        cssModules.set(path, cssStr);
-        const output = `import '${path}';\n ${code}`;
+        cssModules.set(id, cssStr);
+        if (server) {
+          const { moduleGraph, ws } = server;
+          const virtualStyle9 = moduleGraph.getModulesByFile(
+            `\0${VIRTUAL_MODULE_NAME}.css`
+          );
+          if (virtualStyle9) {
+            const seen = new Set();
+            virtualStyle9.forEach(mod =>
+              moduleGraph.invalidateModule(mod, seen)
+            );
+            ws.send({ type: 'full-reload' });
+          }
+          const output = `import '${VIRTUAL_MODULE_NAME}';\n ${code}`;
+          return {
+            code: output,
+            map
+          };
+        }
         return {
-          code: output,
+          code,
           map
         };
       }
       return { code, map };
+    },
+    generateBundle(_, bundles) {
+      let css = '';
+      let id = '';
+      for (const bundle in bundles) {
+        const module = bundles[bundle];
+        if (module.type === 'asset') continue;
+        const { facadeModuleId } = module;
+        if (!id) id = facadeModuleId;
+        if (cssModules.has(facadeModuleId)) {
+          css += cssModules.get(facadeModuleId);
+        }
+      }
+      this.emitFile({
+        name: `${path.parse(id).name}.css`,
+        type: 'asset',
+        source: processCSS(css, { from: undefined }).css
+      });
     }
   };
 };
