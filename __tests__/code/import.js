80e3f9c41ef0bd3b7b9b76c891d0e50dac3d8f40
SukkaW <isukkaw@gmail.com>
2023-02-07 15:47:21 +0800

    test: add more mutation cases

--- __tests__/code/import.js
+++ __tests__/code/import.js
@@ -6,24 +6,3 @@ it('ignores other imports', () => {
   const { code } = compile(input);
   expect(code).toBe(input);
 });
-
-it("support require('style9')", () => {
-  const input = `
-const style9 = require('style9');
-const styles = style9.create({
-  default: {
-    paddingLeft: 2
-  }
-});
-styles('default');
-  `;
-  const { styles, code } = compile(input);
-
-  expect(styles).toBe(`.hhTkCv{padding-left:2px}`);
-  expect(code).toMatchInlineSnapshot(`
-    "const style9 = require('style9');
-
-    const styles = {};
-    \\"hhTkCv \\";"
-  `);
-});
