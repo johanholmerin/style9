9f74cb797cd45cf8afa22b7f4fe580e3df18f743
kanno <812137533@qq.com>
2023-02-24 12:01:15 +0800

    chore: update vite example

--- examples/vite/src/main.js
+++ examples/vite/src/main.js
@@ -8,3 +8,9 @@ const styles = style9.create({
 });
 
 document.body.className = styles('blue');
+
+if (Math.random() > 0.5) {
+  import('./dynamic').then(() => import('./dynamic2'));
+} else {
+  import('./dynamic2').then(() => import('./dynamic'));
+}
