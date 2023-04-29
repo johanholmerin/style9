f7972c00bf5eff5d2ae0f22704125c5c79fca2f9
SukkaW <isukkaw@gmail.com>
2023-04-29 22:55:27 +0800

    test: test the change

---
+++ __tests__/code/fixtures/classes/complex-ternary/output.js
@@ -0,0 +1,18 @@
+import style9 from 'style9';
+const styles = {};
+'cpOcAb ' +
+  ('eNRbYf ' +
+    ('jJMEUn ' +
+      ('jWjrEQ ' +
+        ('eKtERL ' +
+          ('BRobm ' +
+            ('iovjFN ' +
+              ('dIIZah ' +
+                ('hAmKMv ' +
+                  ('dzEaaE ' +
+                    ('eDssNQ ' +
+                      ('jwTRBm ' +
+                        ('haLLXa ' +
+                          ('ljUXOK ' +
+                            ('fUqtKm ' +
+                              (checked ? 'gtDvVO ' : 'nIqxk ')))))))))))))));
