3cfa9678028be58a3d8dc7a4241932adfc3a43c0
SukkaW <isukkaw@gmail.com>
2023-04-30 17:29:29 +0800

    test: simplify the case

--- __tests__/code/fixtures/classes/complex-ternary/code.js
+++ __tests__/code/fixtures/classes/complex-ternary/code.js
@@ -1,51 +1,30 @@
 import style9 from 'style9';
 const styles = style9.create({
-  button: {
-    marginLeft: '0.5rem',
-    position: 'relative',
-    display: 'inline-flex',
-    flexShrink: 0,
+  not_used: {
     padding: '0',
-    height: '19px',
-    width: '35px',
-    borderWidth: '2px',
-    borderColor: 'transparent',
-    borderRadius: '9999px',
-    cursor: 'pointer',
-    transitionDuration: '50ms',
-    transitionTimingFunction: 'cubic-bezier(0.4, 0,0.2, 1)',
-    transitionProperty: 'background-color, border-color, color, fill, stroke',
+    cursor: 'pointer'
+  },
+  base: {
+    pointerEvents: 'none',
     ':focus': {
       outline: 'none'
     }
   },
-  disabled: {
-    backgroundColor: '#9ca3af'
-  },
   enabled: {
-    backgroundColor: '#059669'
+    color: 'green'
   },
-  toggle: {
-    pointerEvents: 'none',
-    display: 'inline-block',
-    height: '15px',
-    width: '15px',
-    borderRadius: '9999px',
-    backgroundColor: '#fff',
-    boxShadow:
-      '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
-    animationDuration: '200ms',
-    lineHeight: '1.5rem',
-    fontSize: '1rem',
-    transitionTimingFunction: 'cubic-bezier(0.4, 0,0.2, 1)',
-    transitionDuration: '50ms'
+  disabled: {
+    color: 'red',
+    cursor: 'not-allowed'
   },
-  toggle__enabled: {
-    transform: 'translateX(1rem)'
+  hover: {
+    ':hover': {
+      color: 'blue'
+    }
   },
-  toggle__disabled: {
-    transform: 'translateX(0)'
+  other: {
+    backgroundColor: 'red'
   }
 });
 
-styles('toggle', checked ? 'toggle__enabled' : 'toggle__disabled');
+styles('base', checked ? 'enabled' : 'disabled', 'hover', 'other');
