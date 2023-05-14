ed3baafe2afa217dee998303022456b9a4c27e3f
kanno <812137533@qq.com>
2023-05-14 15:50:13 +0800

    chore: modify vite plugin type

--- vite.d.ts
+++ vite.d.ts
@@ -1,6 +1,6 @@
 import { Plugin } from 'vite';
-import { FileNameOptions } from './rollup';
+import { CommonOptions } from './rollup';
 
-export type Style9VitePlguinOptions = Partial<FileNameOptions>;
+export type Style9VitePlguinOptions = CommonOptions;
 
 export default function style9Plugin(options?: Style9VitePlguinOptions): Plugin;
